b385c57bbced10baed7304c4420847bbe47c4c26
Andy Mina <mina.andy54@gmail.com>
2022-08-09 15:32:19 -0400

    created chatbox (YB better)

--- init-firebase.js
+++ init-firebase.js
@@ -3,7 +3,13 @@
  * provided for you by firebase after creating your project!
 */
 const firebaseConfig = {
-    
+    apiKey: "AIzaSyB4POInqmv_NrVyqMX7R4FyM72NMKS81v0",
+    authDomain: "firemessages-3aff0.firebaseapp.com",
+    databaseURL: "https://firemessages-3aff0-default-rtdb.firebaseio.com",
+    projectId: "firemessages-3aff0",
+    storageBucket: "firemessages-3aff0.appspot.com",
+    messagingSenderId: "652797262777",
+    appId: "1:652797262777:web:e7526f7f5c2fe4b24dd468"
 };
 
 // initialize the firebase app with this built-in function
